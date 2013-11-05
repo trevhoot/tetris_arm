6ad4de2817342b08043482444d18ab41a3eca6ec
vcoleman <victoria.coleman@students.olin.edu>
2013-11-04 22:32:49 -0500

    Each type of template is seperated into its own node. They can be run in parallel with the tetris_arm.launch. May or may not have improved speed :/

--- scripts/collectKinectPublisher.py
+++ scripts/collectKinectPublisher.py
@@ -10,21 +10,19 @@ import os
 
 #for image analysis
 import cv2
-import cv2 as cv
-import numpy
 import numpy as np
+from cv2 import cv
 
 #to subscribe to and publish images
-from std_msgs.msg import String
 from sensor_msgs.msg import Image
-from tetris_arm.msg import PieceState
+from std_msgs.msg import Int8
+from rospy.numpy_msg import numpy_msg
+from tetris_arm.msg import ProcessedImage
 from cv_bridge import CvBridge, CvBridgeError
 
 
 #for processing test. Delete when ROS communication works
-import pieceTracker as pt
-import pieceIdentify as pi
-  ##<node name="collectKinectNode" pkg="tetris_arm" type="collectKinectNode.py" />
+
 
 
 """
@@ -38,24 +36,27 @@ std_msgs
 
 class image_converter:
 
-  pieceIdentifier = pi.DeterminePiece()
-
   def __init__(self):
     print "init"
 
-    #initiates topics to publish piece state and type to
-    self.pieceState_pub = rospy.Publisher("pieceState", PieceState)
-    self.pieceType_pub = rospy.Publisher("pieceType", String)
+    rospy.init_node('listener', anonymous=True)
 
-    #self.PieceIdentifier = pi.DeterminePiece()
+
+    #initiates topics to publish piece state and type to
+
+    self.processedImage_pub = rospy.Publisher("processedImage", Image)
 
 	 #placeholder for image
-    cv.namedWindow("Image window", 1)
+    cv2.namedWindow("Image window", 1)
 	
     self.bridge = CvBridge()
 	
 	 #subscibes to depth image from kinnect and passes it to callback fn.
     self.image_sub = rospy.Subscriber("camera/depth/image_raw",Image,self.callback) 
+
+    self.imagesub = rospy.Subscriber("camera/depth/image_raw",Image,self.callback)
+
+    rospy.spin()
 
   def callback(self,data):
 
@@ -66,39 +67,32 @@ class image_converter:
     except CvBridgeError, e:
       print e
 
-	 #parse out location RIO (the tredmill area)
-    ##crop_image = cv_image[70:240,100:545]
+	  #parse out location RIO (the tredmill area)
     crop_image = cv_image[60:240,276:565]
-	
-	  #for creating templates
-    #crop_image = crop_image[100:160, 250:330]
+
     templatex=60
     templatey =80
     
 	 #converts image to numpy array and scales the 16bit for depth RIO
 	 #values we are interested in are in the 12000 value range. scale to 250 before convert to 8bit to not lose info
-    crop_image = (numpy.asanyarray(crop_image) -1220) *100
+    crop_image = (np.asanyarray(crop_image) -1220) *100
    
 	  #converts 16mono to 8mono to be threshold processed
-    crop_image = (crop_image/2.**8).astype(numpy.uint8)
+    crop_image = (crop_image/2.**8).astype(np.uint8)
 
-    #crop_image = (crop_image).astype(numpy.uint8)
+    #crop_image = (crop_image).astype(np.uint8)
 
 	  #extracts the areas that are taller than just under the height of the pieces
     whatisthis, thresh1 = cv2.threshold(crop_image, 240, 250, cv2.THRESH_BINARY)
-    print type(thresh1)
-    #for creating template
-    #cv2.imwrite("templates/Field.jpg", thresh1)
 
     #Display image 
-    cv.imshow("Image window", thresh1)
-    cv.waitKey(3)
-
-    pieceList = self.pieceIdentifier.DeterminePieces(thresh1)
-    if (pieceList != []):  
-      x1, y1, theta, pType = pieceList[0]
-      self.pieceState_pub.publish((x1+templatex/2, y1+templatey/2, theta))
-      self.pieceType_pub.publish(pType)	
+    cv2.imshow("Image window", thresh1)
+    #self.bridge.cv_to_imgmsg(thresh1)
+    msg = cv.fromarray(thresh1)
+    msg = self.bridge.cv_to_imgmsg(msg)
+    print type(msg)
+    if (thresh1 != None):
+      self.processedImage_pub.publish(msg)
 
 def main(args):
   ic = image_converter()
@@ -107,7 +101,7 @@ def main(args):
     rospy.spin()
   except KeyboardInterrupt:
     print "Shutting down"
-  cv.destroyAllWindows()
+  cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
