6ff5b84445272e2324591786a5ff309ba31f16e1
Brooks Willis <christopher.willis@students.olin.edu>
2013-12-09 12:33:19 -0500

    renamed ticToMm, found point one of arm location

--- scripts/armOccupancy.py
+++ scripts/armOccupancy.py
@@ -5,15 +5,41 @@ from math import *
 from jointAngles import *
 
 def armSpace(x1,y1,z1,x2,y2,z2):
+	#Find base angles
 	angles1 = jointAngles(x1,y1,z1)
 	angles2 = jointAngles(x2,y2,z2)
+	theta1 = angles1[0]
+	theta2 = angles2[0]
 
-	flat_profile = [154, 790, 245, 432, 155, 279, 63] #mm, from 0,y [+x,-y,-x,+y,+x,+y,-x]
-	occupancy = map_to_coords(flat_profile,angles1[0])
+	#Convert to mm and find extension distance
+	x1 = ticToMm(x1)
+	y1 = ticToMm(y1)
+	z1 = ticToMm(z1)
+	x2 = ticToMm(x2)
+	y2 = ticToMm(y2)
+	z2 = ticToMm(z2)
+	dist1 = sqrt(x1**2 + y1**2 + z1**2)
+	dist2 = sqrt(x2**2 + y2**2 + z2**2)
+
+	length1 = dist1 + 40 #length from base to end of arm
+	length2 = dist2 + 40 #length from base to end of arm
+
+	print length1
+	print length2
+	mapToCoords(length1,theta1,[x1,y1])
+	mapToCoords(length2,theta2,[x2,y2])
 
 def mapToCoords(profile, baseang, offset = [0,0]):
-	
-	p1 = (xmap(profile[0],baseang,offset[0]), ymap(profile[0],baseang,offset[1]))
+	width = 263.5
+	dx = 125
+	dy = 40
+	p1 = (offset[0]+dx*cos(baseang)+dy*cos(pi/2 - baseang),offset[1]-dx*sin(baseang)+dy*sin(pi/2 - baseang))
+	#p2 = (,)
+	#p3 = (,)
+	#p4 = (,)
+	print p1
+
+	#return [p1,p2,p3,p4]
 
 def xMap(delta, baseang, offset):
 	return delta*cos(radians(baseang)) + offset
@@ -21,5 +47,8 @@ def xMap(delta, baseang, offset):
 def yMap(delta, baseang, offset):
 	return delta*sin(radians(baseang)) + offset
 
+def main():
+	armSpace(0,6000,0,4000,4000,0)
 
-
+if __name__ == '__main__':
+	main()
